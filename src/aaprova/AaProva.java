aa14b6ca623f104f693ac6647382f96991b18f7f
agiappichini <a.giappichini@pucciufficio.com>
2021-04-20 03:17:06 +0100

    Primo commit

---
+++ src/aaprova/AaProva.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aaprova;
+
+/**
+ *
+ * @author Admin
+ */
+public class AaProva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
